546c5fe5208649a7265ca47d4c7cdb2d6779da51
Vasili Slapik <vslapik@gmail.com>
2018-03-18 19:28:14 +0300

    Fix function prototypes

--- 09_memory_and_strings/09_str_funcs.h
+++ 09_memory_and_strings/09_str_funcs.h
@@ -1,15 +1,20 @@
-char *strcpy(char *s1, const char *s2)
-char *strncpy(char *s1, const char *s2, size_t n)
-char *strcat(char *s1, const char *s2)
-char *strncat(char *s1, const char *s2, size_t n)
-char *strchr(const char *s, int c)
-char *strrchr(const char *s, int c)
-int strcmp(const char *s1, const char *s2)
-int strncmp(const char *s1, const char *s2, size_t n)
-size_t strspn(char *s1, const char *s2)
-size_t strcspn(char *s1, const char *s2)
-size_t strlen(const char *s)
-char *strpbrk(const char *s1, const char *s2)
-char *strstr(const char *s1, const char *s2)
-char *strtok(char *s1, const char *s2)
+char *strcpy(char * restrict s1, const char * restrict s2);
+char *strcat(char * restrict s1, const char * restrict s2);
+char *strncpy(char * restrict s1, const char * restrict s2,
+              size_t n);
+char *strncat(char * restrict s1, const char * restrict s2,
+              size_t n);
+char *strchr(const char *s, int c);
+char *strrchr(const char *s, int c);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+size_t strspn(const char *s1, const char *s2);
+size_t strcspn(const char *s1, const char *s2);
+size_t strlen(const char *s);
+char *strpbrk(const char *s1, const char *s2);
+char *strstr(const char *s1, const char *s2);
+char *strtok(char * restrict s1, const char * restrict s2);
 char *strerror(int errnum);
+int strcoll(const char *s1, const char *s2);
+size_t strxfrm(char * restrict dest, const char * restrict src, 
+               size_t n);
--- 09_memory_and_strings/mem_funcs.h
+++ 09_memory_and_strings/mem_funcs.h
@@ -1,5 +1,6 @@
-void *memcpy(void *dest, const void *src, size_t n);
+void *memcpy(void * restrict dest, const void * restrict src,
+             size_t n);
 void *memmove(void *dest, const void *src, size_t n);
-void *memchr(const char *s, char c, size_t n);
-int memcmp(const char *s1, const char *s2, size_t n);
-void *memset(char *, int z, size_t);
+void *memchr(const void *s, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void *memset(void *s, int t, size_t n);
